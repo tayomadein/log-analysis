3bc60d35736d13bec9e4ff6bb68d21121471aca6
Tayo Madein <tuby1805@gmail.com>
2017-11-17 00:52:27 +0100

    Run queries for q3

--- log.py
+++ log.py
@@ -48,6 +48,28 @@ def get_popular_authors():
 
   return querydb(query)
 
-dboutput = get_popular_authors()
+def get_error_rate():
+    """Return days were more than 1% of requests lead to errors."""
+
+    query = """
+        select output.date, output.error, output.total, round((output.error*100.00/output.total), 1) as perc
+        from (
+            select response.date, errortable.error, response.total
+            from (
+                select to_char(time, 'YYYY-MM-DD') as date, count (*) as error
+                from log 
+                where status not like '%OK%'
+                group by date) as errortable,
+                (select to_char(time, 'YYYY-MM-DD') as date, count (*) as total
+                from log 
+                group by date) as response
+            where errortable.date = response.date
+            group by response.date, errortable.error, response.total) as output
+        where (output.error*100.00/output.total) > 1.0;
+    """
+    return querydb(query)
+
+dboutput = get_error_rate()
+# get_popular_authors()
 # get_top_articles()
 print dboutput

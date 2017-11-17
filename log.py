4cb8447ec172e7e7ea319da5de79c73ec1a1a653
Tayo Madein <tuby1805@gmail.com>
2017-11-17 01:57:24 +0100

    Print query results

--- log.py
+++ log.py
@@ -6,6 +6,7 @@ import psycopg2
 
 DBNAME = "news"
 
+
 def querydb(query):
     """Handle all db queries"""
     db = psycopg2.connect(database=DBNAME)
@@ -14,54 +15,58 @@ def querydb(query):
     return c.fetchall()
     db.close()
 
+
 def get_top_articles():
-  """Return the top 3 most view articles."""
+    """Return the top 3 most view articles."""
 
-  query = """
-    select title, views
-    from (
-        select path, count (*) as views
-        from log
-        where path like '%article%'
-        group by path) as topthree, articles
-        where topthree.path like '%' || articles.slug || '%'
-        order by views desc
-        limit 3
-  """
+    query = """
+        select title, views
+        from (
+            select path, count (*) as views
+            from log
+            where path like '%article%'
+            group by path) as topthree, articles
+            where topthree.path like '%' || articles.slug || '%'
+            order by views desc
+            limit 3
+    """
 
-  return querydb(query)
+    return querydb(query)
+
 
 def get_popular_authors():
-  """Return all authors in order of popularity."""
+    """Return all authors in order of popularity."""
 
-  query = """
-    select name, sum(views) as author_views 
-    from (
-        select title, author, count (articles.title) as views
-        from log, articles
-        where log.path like '%' || articles.slug || '%'
-        group by title, author) as allviews, authors
-        where allviews.author = authors.id
-        group by name
-        order by author_views desc
-  """
+    query = """
+        select name, sum(views) as author_views
+        from (
+            select title, author, count (articles.title) as views
+            from log, articles
+            where log.path like '%' || articles.slug || '%'
+            group by title, author) as allviews, authors
+            where allviews.author = authors.id
+            group by name
+            order by author_views desc
+    """
 
-  return querydb(query)
+    return querydb(query)
+
 
 def get_error_rate():
     """Return days were more than 1% of requests lead to errors."""
 
     query = """
-        select output.date, output.error, output.total, round((output.error*100.00/output.total), 1) as perc
+        select output.date, output.error, output.total,
+            round((output.error*100.00/output.total), 1) as perc
         from (
             select response.date, errortable.error, response.total
             from (
                 select to_char(time, 'YYYY-MM-DD') as date, count (*) as error
-                from log 
+                from log
                 where status not like '%OK%'
                 group by date) as errortable,
                 (select to_char(time, 'YYYY-MM-DD') as date, count (*) as total
-                from log 
+                from log
                 group by date) as response
             where errortable.date = response.date
             group by response.date, errortable.error, response.total) as output
@@ -69,7 +74,33 @@ def get_error_rate():
     """
     return querydb(query)
 
-dboutput = get_error_rate()
-# get_popular_authors()
-# get_top_articles()
-print dboutput
+
+def print_output():
+    """Print Query results to a text file"""
+
+    print "Getting Log Analysis"
+
+    f = open("output.txt", "w")
+    f.write("Logs Analysis:\n")
+
+    dboutput = get_top_articles()
+    f.write("\n1. Most Popular Articles:\n")
+    for row in dboutput:
+        entry = row[0] + ' - ' + str(row[1]) + " views\n"
+        f.write(entry)
+
+    dboutput = get_popular_authors()
+    f.write("\n\n2. Most Popular Authors:\n")
+    for row in dboutput:
+        entry = row[0] + ' - ' + str(row[1]) + " views\n"
+        f.write(entry)
+
+    dboutput = get_error_rate()
+    f.write("\n\n3. More than 1percent of requests lead to errors on:\n")
+    for row in dboutput:
+        entry = row[0] + ' - ' + str(row[1]) + " views\n"
+        f.write(entry)
+
+    f.close()
+
+print_output()
